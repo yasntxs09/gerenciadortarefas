3c99376ca1d101fda4ce9eef0586783b94e12442
yasntxs09 <yasmim.ana.araujo@escola.pr.gov.br>
2026-07-28 12:31:58 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Footer from './componentes/Footer';
+
+function App() {
+  return (
+   <Footer/>
+  );
+}
+
+export default App;
---
+++ src/componentes/Footer/index.js
@@ -0,0 +1,21 @@
+import './index.css'
+
+function Footer(){
+    return (
+        <footer>
+        <div className="colegio"> 
+            <p className='nomeColegio'>
+                Colégio Santa Maria Goretti - 2026
+            </p>
+            
+            </div>
+        <div className="desenvolvedor">
+            <p className='nomeDesenvolvedor'>
+                Desenvolvido por:Yasmim Santana 
+            </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
